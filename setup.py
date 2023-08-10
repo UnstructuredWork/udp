759704e534fc1abe89e5b87fdcec4da1dc1bec83
sunghyun-nam <tjdgus6255@naver.com>
2023-08-10 13:33:28 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='udp',
+    version='0.1.0',
+    author="SungHyun Nam",
+    author_email="tjdgus6255@keti.re.kr",
+    packages=find_packages(include=['udp', 'udp.*']),
+    python_requires='>=3.7',
+    install_requires=[
+        'opencv-python',
+        'opencv-contrib-python',
+        'pynvjpeg',
+    ]
+)
+
+
+
+
+
