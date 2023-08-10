5cc1441230fb6a72afda1a0a605c624dd356340a
sunghyun-nam <tjdgus6255@naver.com>
2023-08-10 14:06:23 +0900

    initial commit

--- udp/__init__.py
+++ udp/__init__.py
@@ -1,7 +1,13 @@
+from .client import Client
 from .rgbd_client import RgbdClient
 from .stereo_client import StereoClient
+from .parallel import thread_method, process_method
+
 
 __all__ = [
+    "Client",
     "RgbdClient",
     "StereoClient",
+    "thread_method",
+    "process_method",
 ]
